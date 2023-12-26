93469958b769780ad6c99598568518e2cca91413
조하은 <joha-eun@joha-eun-ui-MacBookAir.local>
2023-12-26 22:15:58 +0900

    Add 1-2.py

--- coding-interview/data_structure/string/1-2.py
+++ coding-interview/data_structure/string/1-2.py
@@ -1,12 +1,25 @@
-from typing import Boolean
-
-def is_permutation(s1: str, s2: str) -> Boolean:
+# 순열 관계?
+# abcd, bcda -> True
+# abbcd, bcda -> False
+# cdefg, abcde -> False
+def is_permutation(s1: str, s2: str) -> bool:
     """
     Given two strings, write a method to decide if one is a permutation of the
     other.
     """
-    pass
+    if len(s1) == len(s2):
+        s1 = sorted(s1)
+        s2 = sorted(s2)
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                return False
+        return True
+    
+    else:
+        return False
 
 if __name__ == '__main__':
     # Write your test cases here
-    pass
+    assert is_permutation("abcd", "bcda") == True
+    assert is_permutation("shdf", "sdfg") == False
+    assert is_permutation("opqr", "opqrs") == False
