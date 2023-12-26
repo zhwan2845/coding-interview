68c471e1f1f8faa9670a29944d5b6998514aaa99
조하은 <joha-eun@joha-eun-ui-MacBookAir.local>
2023-12-26 22:28:40 +0900

    Add 1-4.py

--- coding-interview/data_structure/string/1-4.py
+++ coding-interview/data_structure/string/1-4.py
@@ -1,13 +1,20 @@
-from typing import Boolean
-
-def is_palindrome(s: str) -> Boolean:
+def is_palindrome(s: str) -> bool:
     """
     Given a string, write a function to check if it is a permutation of a
     palindrome. A palindrome is a word or phrase that is the same forwards and
     backwards. A permutation is a rearrangement of letters.
     """
-    pass
+    s = s.replace(" ", "")
+    s = s.lower()
+    for i in range(len(s)//2):
+        if s[i] != s[len(s) - i - 1]:
+            return False
+    return True
 
 if __name__ == '__main__':
     # Write your test cases here
-    pass
+    assert is_palindrome("abcba") == True
+    assert is_palindrome("abccba") == True
+    assert is_palindrome("abcdef") == False
+    assert is_palindrome("taco cat") == True
+    assert is_palindrome("Tac cat") == True
