c8396dd54866327ddd8ec28c1ce1cc3980bd48a4
zhwan2845 <johaeun0802@naver.com>
2024-01-09 20:25:21 +0900

    Add linked_list.py

--- data_structure/linked_list.py
+++ data_structure/linked_list.py
@@ -7,3 +7,80 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.num_of_nodes = 0
+
+    def insert_first(self, data: int):
+        # 연결 리스트가 비어있는 경우
+        if self.head == None:
+            self.head = Node(data, None)
+        # 연결 리스트가 비어있지 않은 경우
+        else:
+            node = Node(data, self.head)
+            self.head = node
+        self.num_of_nodes += 1
+
+    def insert_last(self, data: int):
+        # 연결 리스트가 비어있는 경우
+        if self.head == None:
+            self.head = Node(data, None)
+        # 연결 리스트가 비어있지 않은 경우
+        else:
+            node = Node(data, None)
+            cur_node = self.head
+            while cur_node.next != None:
+                cur_node = cur_node.next
+            cur_node.next = node
+        self.num_of_nodes += 1
+
+    def delete_first(self):
+        # 연결 리스트가 비어있는 경우
+        if self.head is None:
+            return
+
+        self.head = self.head.next
+        self.num_of_nodes -= 1
+
+
+    def delete_last(self):
+        # 연결 리스트가 비어있는 경우
+        if self.head is None:
+            return
+
+        # 마지막 노드를 삭제할 때
+        if self.head.next is None:
+            self.head = None
+        else:
+            cur_node = self.head
+            while cur_node.next.next is not None:
+                cur_node = cur_node.next
+            cur_node.next = None
+        self.num_of_nodes -= 1
+
+    def print(self):
+        cur_node = self.head
+        print(f"({self.num_of_nodes}) ")
+        while cur_node != None:
+            print(f"{cur_node.data} ->", end=" ")
+            cur_node = cur_node.next
+        print()
+
+if __name__ == '__main__':
+    # TODO: 1/9까지 linked list의 모든 코드가 실행될 수 있는 테스트 코드 작성하기 (coding-interview-2)
+    ll = LinkedList()
+    ll.insert_first(3)
+    ll.insert_first(1)
+    ll.insert_last(7)
+    ll.insert_last(5)
+    ll.print()
+    ll.delete_first()
+    ll.print()
+    ll.delete_last()
+    ll.print()
+    ll.delete_first()
+    ll.print()
+    ll.delete_first()
+    ll.print()
+    ll.delete_last()
+    ll.print()
+
+    
+    
