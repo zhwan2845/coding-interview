6b433f11343b6ab5eb416636348fe653521fc2b2
zhwan2845 <johaeun0802@naver.com>
2024-01-17 20:15:49 +0900

    [WIP] Add files

--- coding_interview/2-1.py
+++ coding_interview/2-1.py
@@ -1,11 +1,27 @@
-from ..data_structure.linked_list import LinkedList
+from data_structure.linked_list import LinkedList
 
 def delete_duplicates(ll: LinkedList):
     """
     Remove duplicates from an unsorted linked list.
     """
-    pass
+    if ll.head is None:
+        return
+
+    cur_node = ll.head
+    seen_values = {cur_node.data}
+
+    while cur_node.next:
+        if cur_node.next.data == seen_values:
+            cur_node.next = cur_node.next.next
+        else:
+            seen_values.add(cur_node.next.data)
+            cur_node = cur_node.next
 
 if __name__ == '__main__':
-    # Write your test cases here
-    pass
+    linked_list = LinkedList()
+    elements = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+    for element in elements:
+        linked_list.append(element)
+
+    delete_duplicates(linked_list)
+    linked_list.display()
--- coding_interview/2-2.py
+++ coding_interview/2-2.py
@@ -4,7 +4,26 @@ def nth_to_last(ll: LinkedList, n: int):
     """
     Return nth to last element of a singly linked list.
     """
-    pass
+    if ll.head == None:
+        return None
+    if n <= 0:
+        return None
+
+    slow = fast = ll.head
+
+    # Move fast pointer n nodes ahead
+    for _ in range(n):
+        if fast is None:
+            return None  # n이 리스트의 길이보다 클 경우
+
+        fast = fast.next
+
+    # Move both pointers until fast reaches the end
+    while fast.next:
+        slow = slow.next
+        fast = fast.next
+
+    return slow.data
 
 if __name__ == '__main__':
     # Write your test cases here
